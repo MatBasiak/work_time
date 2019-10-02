9b9a21a96c63001b80970b9f284af988a72dd9f4
mateusz <mateusz.basiak@gmail.com>
2019-10-02 14:36:06 +0200

    fixed timediffernce function

--- src/layout/header.js
+++ src/layout/header.js
@@ -26,28 +26,35 @@ export default class Header extends Component {
    * @param {string} start '11:11'
    * @param {string} end   '11:11'
    * convert them to array of strings
-   * calculate  difference between each elements of array
-   * and return array of numbers
-   * result.reduce combine array to single element
+   
    *@return result string 11:11
    */
   timeDifference(start, end) {
     start = [...start];
     end = [...end];
-    let result = start.map((item, i) => {
-      if (item === ":") {
-        return ":";
-      }
-      return item - end[i].toString();
-    });
-    result = result.reduce((c, i) => c + i);
-    return result;
+
+    let startHours = start[0].concat(start[1]);
+    let startMinutes = start[3].concat(start[4]);
+    let endHours = end[0].concat(end[1]);
+    let endMinutes = end[3].concat(end[4]);
+    let resultHours = endHours - startHours;
+    let resultMinutes = endMinutes - startMinutes;
+
+    if (startMinutes > endMinutes) {
+      resultMinutes = 60 - -resultMinutes;
+      resultHours--;
+    }
+    if (+endHours >= 0 && +endHours < startHours) {
+      resultHours = 24 - startHours + +endHours;
+    }
+
+    return `${resultHours}:${resultMinutes}`;
   }
 
   clickHandler() {
     let result = this.timeDifference(
-      this.state.endValue,
-      this.state.startValue
+      this.state.startValue,
+      this.state.endValue
     );
     this.setState({
       result
