76e6eff017305d3920456e32d229d617b8653513
mateusz <mateusz.basiak@gmail.com>
2019-10-03 19:45:24 +0200

    Create date component
    use momentjs lib for easier date formating

---
+++ src/components/date.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import React, { Component } from "react";
+import moment from "moment";
+
+const Container = styled.div`
+    color: blue;
+    font-size: 25px;
+    display: block;
+
+    border: 1px solid green;
+    p {
+        padding: 0;
+        margin: 0;
+        text-align: right;
+        font-size: 36px;
+    }
+`;
+
+class DateDisplay extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date()
+        };
+    }
+    componentWillMount() {
+        this.tick();
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+    render() {
+        return (
+            <Container>
+                <p>{moment().format("DD:MM:YYYY")}</p>
+            </Container>
+        );
+    }
+}
+
+export default DateDisplay;
--- src/layout/header.js
+++ src/layout/header.js
@@ -3,27 +3,28 @@ import styled from "styled-components";
 import Button from "../components/button";
 import Input from "../components/input";
 
+import DateDisplay from "../components/date";
+
 const Container = styled.div`
-  background-color: red;
-  color: white;
+    background-color: red;
+    color: white;
 `;
 
 export default class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      date: new Date(),
-      startValue: "11:11",
-      endValue: "22:22",
-      result: " ",
-      list: []
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            startValue: "11:11",
+            endValue: "22:22",
+            result: " ",
+            list: []
+        };
 
-    this.clickHandler = this.clickHandler.bind(this);
-    this.setTime = this.setTime.bind(this);
-  }
+        this.clickHandler = this.clickHandler.bind(this);
+        this.setTime = this.setTime.bind(this);
+    }
 
-  /**
+    /**
    *
    * @param {string} start '11:11'
    * @param {string} end   '11:11'
@@ -31,91 +32,91 @@ export default class Header extends Component {
    
    *@return result string 11:11
    */
-  timeDifference(start, end) {
-    start = [...start];
-    end = [...end];
+    timeDifference(start, end) {
+        start = [...start];
+        end = [...end];
 
-    let startHours = start[0].concat(start[1]);
-    let startMinutes = start[3].concat(start[4]);
-    let endHours = end[0].concat(end[1]);
-    let endMinutes = end[3].concat(end[4]);
-    let resultHours = endHours - startHours;
-    let resultMinutes = endMinutes - startMinutes;
+        let startHours = start[0].concat(start[1]);
+        let startMinutes = start[3].concat(start[4]);
+        let endHours = end[0].concat(end[1]);
+        let endMinutes = end[3].concat(end[4]);
+        let resultHours = endHours - startHours;
+        let resultMinutes = endMinutes - startMinutes;
 
-    if (startMinutes > endMinutes) {
-      resultMinutes = 60 - -resultMinutes;
-      resultHours--;
+        if (startMinutes > endMinutes) {
+            resultMinutes = 60 - -resultMinutes;
+            resultHours--;
+        }
+        if (+endHours >= 0 && +endHours < startHours) {
+            resultHours = 24 - startHours + +endHours;
+        }
+
+        return `${resultHours}:${resultMinutes}`;
     }
-    if (+endHours >= 0 && +endHours < startHours) {
-      resultHours = 24 - startHours + +endHours;
-    }
 
-    return `${resultHours}:${resultMinutes}`;
-  }
-
-  clickHandler() {
-    let result = this.timeDifference(
-      this.state.startValue,
-      this.state.endValue
-    );
-    this.setState({
-      result
-    });
-
-    this.setState(state => {
-      const list = state.list.concat(state.result);
-      return {
-        list
-      };
-    });
-    console.log(this.state);
-  }
-  /**
-   *
-   * @param {event} e
-   * function setting to state value of inputs
-   */
-  setTime(e) {
-    switch (e.target.name) {
-      case "start":
+    clickHandler() {
+        let result = this.timeDifference(
+            this.state.startValue,
+            this.state.endValue
+        );
         this.setState({
-          startValue: e.target.value
+            result
         });
-        break;
-      case "end":
-        this.setState({
-          endValue: e.target.value
-        });
-        break;
 
-      default:
-        break;
+        this.setState(state => {
+            const list = state.list.concat(state.result);
+            return {
+                list
+            };
+        });
+        console.log(this.state);
     }
-  }
+    /**
+     *
+     * @param {event} e
+     * function setting to state value of inputs
+     */
+    setTime(e) {
+        switch (e.target.name) {
+            case "start":
+                this.setState({
+                    startValue: e.target.value
+                });
+                break;
+            case "end":
+                this.setState({
+                    endValue: e.target.value
+                });
+                break;
 
-  render() {
-    return (
-      <Container>
-        start
-        <Input
-          name="start"
-          type="time"
-          value={this.state.startValue}
-          onChange={e => this.setTime(e)}
-        />
-        end
-        <Input
-          name="end"
-          type="time"
-          value={this.state.endValue}
-          onChange={e => this.setTime(e)}
-        />
-        <Button onClick={this.clickHandler}>ADD</Button>
-        <p>{this.state.startValue}</p>
-        <p>{this.state.endValue}</p>
-        <p>{this.state.result}</p>
-        <p>{this.state.date.toLocaleDateString()}</p>
-      </Container>
-    );
-  }
+            default:
+                break;
+        }
+    }
+
+    render() {
+        return (
+            <Container>
+                <DateDisplay />
+                start
+                <Input
+                    name="start"
+                    type="time"
+                    value={this.state.startValue}
+                    onChange={e => this.setTime(e)}
+                />
+                end
+                <Input
+                    name="end"
+                    type="time"
+                    value={this.state.endValue}
+                    onChange={e => this.setTime(e)}
+                />
+                <Button onClick={this.clickHandler}>ADD</Button>
+                <p>{this.state.startValue}</p>
+                <p>{this.state.endValue}</p>
+                <p>{this.state.result}</p>
+            </Container>
+        );
+    }
 }
